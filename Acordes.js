2a8297dbf7cba8c8ce7a09592efd2b21844fa310
cparauta <claudio_parauta@hotmail.com>
2020-04-27 16:54:50 +0100

    Versão Teste

---
+++ Acordes.js
@@ -0,0 +1,101 @@
+var selectedPreset = _tone_0250_Acoustic_Guitar_sf2_file;
+var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContextFunc();
+//var output = audioContext.destination;
+var player = new WebAudioFontPlayer();
+//var now = 0;
+player.adjustPreset(audioContext, selectedPreset);
+function TOCA(pitches) {
+  var audioBufferSourceNode = player.queueChord(audioContext, audioContext.destination, selectedPreset, audioContext.currentTime + 0, pitches, 1.5, 1)
+}
+
+var C = 0, Cs = 1, D = 2, Ds = 3, E = 4, F = 5, Fs = 6, G = 7, Gs = 8, A = 9, As = 10, B = 11;
+var O = 12;
+var _6th = E + O * 3, _5th = A + O * 3, _4th = D + O * 4, _3rd = G + O * 4, _2nd = B + O * 4, _1st = E + O * 5;
+
+var fretsAm = [-1, 0, 2, 2, 1, 0];
+var fretsC = [-1, 3, 2, 0, 1, 0];
+var fretsE = [0, 2, 2, 1, 0, 0];
+var fretsG = [3, 2, 0, 0, 0, 3];
+var fretsDm = [-1, -1, 0, 2, 3, 1];
+
+function pitches(frets) {
+  var p = [];
+  if (frets[0] > -1) p.push(_6th + frets[0]);
+  if (frets[1] > -1) p.push(_5th + frets[1]);
+  if (frets[2] > -1) p.push(_4th + frets[2]);
+  if (frets[3] > -1) p.push(_3rd + frets[3]);
+  if (frets[4] > -1) p.push(_2nd + frets[4]);
+  if (frets[5] > -1) p.push(_1st + frets[5]);
+  return p;
+}
+
+
+
+//var notes = [C, D];
+let pieColors = [0, 30, 60, 90, 120, 150, 180];
+//let osc;
+//let playing = false;
+let song;
+let diameter = 300;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //song.loop(); // song is ready to play during setup() because it was loaded during preload
+  noStroke();
+  fullscreen();
+}
+
+// extract peaks from the FFT and then create a HPCP
+
+function draw() {
+  background(255);
+  pieChart(diameter);
+}
+
+
+function pieChart(diameter) {
+  let lastAngle = HALF_PI;
+  for (let i = 0; i < 7; i++) {
+    fill(pieColors[i]);
+    arc(width / 2, height / 2, diameter, diameter, lastAngle, lastAngle + TWO_PI / 7);
+    lastAngle += TWO_PI / 7;
+  }
+}
+
+
+function mousePressed() {
+  // Check if mouse is inside the circle!
+  let d = dist(mouseX, mouseY, width / 2, height / 2);
+  if (d < diameter / 2) {
+    //startTime = new Date().getSeconds();
+    // Compute which slice was clicked!
+    let s = int(map(abs(atan2(mouseX - width / 2, mouseY - height / 2) - TWO_PI) % TWO_PI, 0, TWO_PI, 0, 7));
+    print(s);
+    if (s == 0) {
+      TOCA(pitches(fretsAm));
+    } else if (s == 1) {
+      TOCA(pitches(fretsC));
+    } else if (s == 2) {
+      TOCA(pitches(fretsG));
+    } else if (s == 3) {
+      TOCA(pitches(fretsDm));
+    } else if (s >= 4) {
+      TOCA(pitches(fretsE));
+    }
+    //print(playing);
+  }
+}
+
+function mouseReleased() {
+  //endTime = new Date().getSeconds();
+  //total = (endTime - (startTime)) * 1000;
+  //print("CIMA")
+  //let duration = total
+  //console.log(total + " Seconds")
+}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  diameter = min(width, height);
+}
---
+++ analise-DESKTOP-663OGRV.js
@@ -0,0 +1,125 @@
+//NOTAS//
+var C = 0, Cs = 1, D = 2, Ds = 3, E = 4, F = 5, Fs = 6, G = 7, Gs = 8, A = 9, As = 10, B = 11;
+var O = 12*5 ;
+let notes = [Gs+O, C+O, Ds+O, G+O, As+O, Cs+O, Fs+O];//1;3;5;7;2;4;6//
+var pieColors = [[cor1], [cor2]] ;//Cores estão de preto para cinza [0, 30, 60, 90, 120, 150, 180]
+var cor1 =[]
+var cor2 = []
+var song;
+var button;
+//let playing = false;
+let diameter = 300;
+let table
+
+
+
+//Para o webaudiofont funcionar//
+var selectedPreset = _tone_0000_SBLive_sf2;
+var AudioContextFunc = window.AudioContext || window.webkitAudioContext;
+var audioContext = new AudioContextFunc();
+//var output = audioContext.destination;
+var player = new WebAudioFontPlayer();
+//var now = 0;
+player.adjustPreset(audioContext, selectedPreset);
+
+
+//Preload//
+function preload() {
+    pitch = loadTable('pitch.txt', 'tsv');
+    song = loadSound("guitarwithecho.wav");
+    table = loadTable('analysis.txt', 'csv');
+}
+
+
+
+//Setup//
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  //song.loop(); // song is ready to play during setup() because it was loaded during preload
+  noStroke();
+  fullscreen();
+  button = createButton("play");
+  button.mousePressed(tooglePlaying)
+  // row number
+
+  //cycle through all values in the row -- start in the second
+  for (let c = 1; c < table.getColumnCount(); c++) {
+    //print(table.getString(r,c)*255);
+    //pieColors = table.getString(r,c)*255
+    cor1.push(table.getString(0,c)*255)
+    //print(cor1)
+    cor2.push(table.getString(1,c)*255)
+    console.log(cor1[0])
+    //print(cor2)
+    //print(pieColors[0])
+  }
+  
+
+
+
+  //extrair valores 
+  //let row = table.getRow(0);
+  //print it column by column
+  //note: a row is an object, not an array
+  //for (let c = 0; c < table.getColumnCount(); c++) {
+    //pieColors = row.getString(c)
+   //print(row.getString(c));
+    //print(pieColors)
+
+  }
+
+function draw() {
+  background(255);
+  pieChart(diameter);
+}
+
+// Funcao para o webaudiofont //
+function startWaveTableNow(pitch, duration) {
+  var audioBufferSourceNode = player.queueWaveTable(audioContext, audioContext.destination, selectedPreset, audioContext.currentTime + 0, pitch, duration, 0.3)
+}
+
+function tooglePlaying() {
+  if (!song.isPlaying()) {
+    song.play();
+    song.setVolume(0.5);
+    button.html("stop");  
+  } else {
+    song.stop();
+    button.html("play");
+  } 
+}
+
+function pieChart(diameter) {
+  let lastAngle = HALF_PI;
+  for (let i = 0; i < 7; i++) {
+    fill(cor2[i]);
+    arc(width / 2, height / 2, diameter, diameter, lastAngle, lastAngle + TWO_PI / 7);
+    lastAngle += TWO_PI / 7;
+  }
+}
+
+
+function mousePressed() {
+  // Check if mouse is inside the circle!
+  let d = dist(mouseX, mouseY, width / 2, height / 2);
+  if (d < diameter / 2) {
+    //startTime = new Date().getSeconds();
+    // Compute which slice was clicked!
+    let s = int(map(abs(atan2(mouseX - width / 2, mouseY - height / 2) - TWO_PI) % TWO_PI, 0, TWO_PI, 0, 7));
+    //print(s);
+    startWaveTableNow(notes[s], 1)
+    //print(playing);
+  }
+}
+
+//function mouseReleased() {
+  //endTime = new Date().getSeconds();
+  //total = (endTime - (startTime)) * 1000;
+  //print("CIMA")
+  //let duration = total
+  //console.log(total + " Seconds")
+//}
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  diameter = min(width, height);
+}
